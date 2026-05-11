43fc3f4f33896cd29e17dc2014860faec40e693d
zpnb78177 <zpnb78177@gmail.com>
2026-05-11 16:02:04 +0800

    Add files via upload

---
+++ 03.输入与输出.py
@@ -0,0 +1,37 @@
+
+#input
+# name=input("请输入你的名字：")
+# age=input("请输入你的年龄：")
+# print(f"你姓名是{name}，年龄是{age}")
+
+# # 银行卡ATM取款
+# all_money=10000
+# coorect_password=123456
+# password=int(input("请输入银行卡密码："))#将string类型转为int类型
+# if password == coorect_password:
+#     money=input("密码正确！\n请输入取款金额：")#\n是换行符
+#     account_money=all_money-money
+#     if account_money>=0:
+#         print(f"取款成功！\n你的银行卡余额为:{account_money}")
+#     else:
+#         print("取款失败，余额不足！")
+# else:
+#     print("密码错误！")
+
+#根据用户输入的两个数字，计算两个数之和，并将其输出到控制台
+# num1=int(input("请输入第一个数字："))
+# num2=int(input("请输入第二个数字："))
+# print(f"两数之和为：{num1+num2}")
+
+# num1=input("请输入第一个数字：")
+# num2=input("请输入第二个数字：")
+# sum=int(num1)+int(num2)
+# print(f"两数之和为：{sum}")
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print(2//2)
+print(2%2)
+print(2**2)
